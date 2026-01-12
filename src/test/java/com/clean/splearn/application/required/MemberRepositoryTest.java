a7250172f73cf7e2e6672380a630e9ca345a2e62
jangjisu <jjs62611615@gmail.com>
2026-01-12 20:00:43 +0900

    [feat] 회원 이메일 자연키 지정

--- src/test/java/com/clean/splearn/application/required/MemberRepositoryTest.java
+++ src/test/java/com/clean/splearn/application/required/MemberRepositoryTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import static com.clean.splearn.domain.MemberFixture.createMemberRegisterRequest;
 import static com.clean.splearn.domain.MemberFixture.createPasswordEncoder;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
@@ -31,5 +33,21 @@ class MemberRepositoryTest {
 
         // then
         assertNotNull(savedMember.getId());
+    }
+
+    @DisplayName("같은 이메일을 가진 유저를 생성할 경우 실패한다.")
+    @Test
+    void duplicateEmailFail() {
+        // given
+        Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        memberRepository.save(member);
+
+        // when
+        Member member2 = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+
+        // then
+        assertThatThrownBy(() -> memberRepository.save(member2))
+                .isInstanceOf(DataIntegrityViolationException.class);
+
     }
 }
