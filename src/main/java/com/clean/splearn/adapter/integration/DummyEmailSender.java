f84313dd4bfb06e7b845230d9c046d8289b6103a
jangjisu <jjs62611615@gmail.com>
2026-01-19 13:41:01 +0900

    [feat] DummyEmailSender @Fallback (가장 나중에 사용되어야 될 빈) 정보 추가

--- src/main/java/com/clean/splearn/adapter/integration/DummyEmailSender.java
+++ src/main/java/com/clean/splearn/adapter/integration/DummyEmailSender.java
@@ -2,9 +2,11 @@ package com.clean.splearn.adapter.integration;
 
 import com.clean.splearn.application.required.EmailSender;
 import com.clean.splearn.domain.Email;
+import org.springframework.context.annotation.Fallback;
 import org.springframework.stereotype.Component;
 
 @Component
+@Fallback
 public class DummyEmailSender implements EmailSender {
     @Override
     public void send(Email email, String subject, String body) {
