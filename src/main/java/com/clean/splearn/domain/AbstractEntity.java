de7653c7a1ea0d7d45f4b156c57c4abd757087b0
jangjisu <jjs62611615@gmail.com>
2026-01-20 20:11:01 +0900

    [fix] Intelij 경고 줄 해결을 위한 MappedSuperClass 어노테이션 추가

--- src/main/java/com/clean/splearn/domain/AbstractEntity.java
+++ src/main/java/com/clean/splearn/domain/AbstractEntity.java
@@ -3,8 +3,10 @@ package com.clean.splearn.domain;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
+@MappedSuperclass
 public abstract class AbstractEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
